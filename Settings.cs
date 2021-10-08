855a094e31ed5a532bf1b91781aa84fb2638ef7b
Zeke Juel <Zeke.Juel@inclusa.org>
2021-10-08 10:51:33 -0500

    Added page view settings
    Moved code, parameters, and functions out of Razor file.

---
+++ Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp2.Shared
+{
+    public class Settings
+    {
+        private int _maxRowsToDisplay;
+        public int maxRowsToDisplay {
+
+            get { return _maxRowsToDisplay; } 
+
+            set { if (value < 10) _maxRowsToDisplay = 10;
+                else if (value > 200) _maxRowsToDisplay = 200;
+                else _maxRowsToDisplay = value;
+            }
+        }
+
+        public Settings()
+        {
+            maxRowsToDisplay = 10;
+        }
+    }
+}
---
+++ Shared/ViewDataTable.razor.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class ViewDataTable_Code: ComponentBase
+    {
+        [Parameter]
+        public System.Data.DataTable table { get; set; }
+
+        public Settings settings = new Settings();
+
+        string lastSortedColumn = "";
+        bool sort = true;
+        public int pages = 1;
+        public int currentpage = 1;
+
+        public string PreviousPage_Name = "Previous Page";
+        public string NextPage_Name = "Next Page";
+
+        public void PreviousPage()
+        {
+            currentpage--;
+        }
+        public void NextPage()
+        {
+            currentpage++;
+        }
+
+        public ViewDataTable_Code()
+        {
+            if(!(table is null))
+            {
+                pages = (table.Rows.Count / settings.maxRowsToDisplay) + 1;
+
+            }
+        }
+        public void Sort(string columnName)
+
+        {
+            DataView prodView = table.DefaultView;
+
+            if (lastSortedColumn == columnName)
+            {
+                //Update key value
+                sort = !sort;
+            }
+            else
+            {
+                lastSortedColumn = columnName;
+                sort = true;
+            }
+
+            if (sort)
+            {
+                prodView.Sort = $"{columnName} DESC";
+            }
+            else
+            {
+                prodView.Sort = $"{columnName} ASC";
+            }
+            table = prodView.ToTable();
+
+        }
+
+        public void HandleValidSubmit()
+        {
+            Console.WriteLine("asd");// Save User object to backend
+        }
+    }
+}
