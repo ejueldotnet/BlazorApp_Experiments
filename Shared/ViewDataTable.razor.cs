194529357ebea1ef37ee994a578a0e3b605a3d06
Zeke Juel <Zeke.Juel@inclusa.org>
2021-10-08 15:49:06 -0500

    Added Pop-Up functionality

--- Shared/ViewDataTable.razor.cs
+++ Shared/ViewDataTable.razor.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BlazorApp2.Shared
@@ -14,6 +16,8 @@ namespace BlazorApp2.Shared
         public System.Data.DataTable table { get; set; }
 
         public Settings settings = new Settings();
+        public string Message { get; set; }
+        public string MessageHeader { get; set; }
 
         string lastSortedColumn = "";
         bool sort = true;
@@ -35,7 +39,8 @@ namespace BlazorApp2.Shared
 
         public ViewDataTable_Code()
         {
-            if(!(table is null))
+            Message = "";
+            if (!(table is null))
             {
                 pages = (table.Rows.Count / settings.maxRowsToDisplay) + 1;
 
@@ -72,6 +77,36 @@ namespace BlazorApp2.Shared
         public void HandleValidSubmit()
         {
             Console.WriteLine("asd");// Save User object to backend
+        }
+
+        public async Task SetDataTableValueAsync(DataRow row,DataColumn column, ChangeEventArgs e)
+        {
+            //Use to convert value to column type
+            if (column.DataType.Name == "DateTimeOffset")
+            {
+                //This is the only field type I've found that doesn't naturaly convert from Excel to the db fields
+                row[column] = DateTimeOffset.Parse(e.Value.ToString());
+            }
+            else
+            {
+                try
+                {
+                    row[column] = e.Value;
+
+                }
+                catch(Exception ex)
+                {
+                    Alert("Error Occured",ex.Message);
+                    e.Value = row[column];
+                }
+            }
+
+        }
+        private async Task Alert(string theMessageHeader, string theMessage)
+        {
+            MessageHeader = theMessageHeader;
+            Message = theMessage;
+            //await JSRuntime.InvokeAsync<object>("Alert", new object(){message});
         }
     }
 }
